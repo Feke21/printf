8135ca52e9253467fd3646256994c2aacc488d31
Feke_21 <favouredubio@gmail.com>
2023-01-24 21:31:41 +0000

    Fixing files

--- pointer.c
+++ pointer.c
@@ -36,6 +36,41 @@ int _puts(char *str, int ascii)
 }
 
 /**
+ * convert_base - convert unsigned from base 10 to base
+ * @nb: decimal number
+ * @base: base to convert from
+ * @upper: upper case check
+ *
+ * Return: converted number to string
+ */
+
+char *convert_base(unsigned long nb, unsigned int base, int upper)
+{
+	int i = 0;
+	char *str;
+	unsigned long nbr = nb;
+
+	while (nbr >= base)
+	{
+		nbr /= base;
+		i++;
+	}
+	str = malloc(sizeof(char) * i + 2);
+	if (!str)
+		return (0);
+	str[i + 1] = '\0';
+
+	while (i >= 0)
+	{
+		nbr = nb % base;
+		str[i] = convert_alpha_numeric(nbr, upper);
+		nb /= base;
+		i--;
+	}
+	return (str);
+}
+
+/**
  * convert_base_pointer - convert pointer void to ul
  * @p: pointer
  *
