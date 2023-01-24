8bb090ce032365b83cecaddba285cb9e9c884c16
Feke_21 <favouredubio@gmail.com>
2023-01-24 11:06:50 +0000

    Update: header file

--- main.h
+++ main.h
@@ -5,5 +5,20 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list);
+int print_str(va_list);
+int print_str(va_list);
+int print_percent(va_list);
+int print_number(va_list);
+int print_number(va_list);
+int print_unsigned_int(va_list);
+int print_octal(va_list);
+int print_lower_hexa(va_list);
+int print_upper_hexa(va_list);
+int print_pointer(va_list);
+int print_reverse(va_list);
+int print_rot13(va_list);
+int print_str_unprintable(va_list);
 
 #endif
