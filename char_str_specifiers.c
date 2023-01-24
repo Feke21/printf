44589021206e0f0a006a575b85b9bbd194b6228f
Feke_21 <favouredubio@gmail.com>
2023-01-24 12:25:03 +0000

    Adding str and char specifiers

--- char_str_specifiers.c
+++ char_str_specifiers.c
@@ -2,21 +2,23 @@
 
 /**
  * print_char - print character to stdout
+ *@list: list of arguments
  *
  * Return: @print_char
  */
-int print_char(va_list)
+int print_char(va_list list)
 {
-	_putchar(va_arg(args,int));
+	_putchar(va_arg(args, int));
 	return (1);
 }
 
 /**
  * print_str - print string to stdout
+ * @list: list of arguments
  *
  * Return: @print_str
  */
-int print_str(va_list)
+int print_str(va_list list)
 {
 	int i;
 	char *str;
