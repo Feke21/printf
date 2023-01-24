aab70d1eb4eb9207fc9cbde4ee87c10c7218e920
Its-Ajua <akinladerebecca04@gmail.com>
2023-01-24 15:17:36 -0800

    Add

--- main.h
+++ main.h
@@ -43,5 +43,6 @@ int print_str_unprintable(va_list ar_list);
 char *convert_rot13(char *str);
 int _strlen_recursion(char *s);
 int convert_alpha_numeric(int nb, int upper);
+int main(void);
 
 #endif
