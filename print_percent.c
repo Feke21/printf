8c801dd90e5659f0f85caf54074a91ed4f7dcad5
Feke_21 <favouredubio@gmail.com>
2023-01-25 00:08:47 +0000

    Update: header file

--- print_percent.c
+++ print_percent.c
@@ -6,7 +6,7 @@
  *
  * Return: percent char
  */
-int print_percent(__attribute__((unused))va_list ar_list)
+int print_percent(__attribute__((unused)) va_list ar_list)
 {
 	_putchar('%');
 	return (1);
